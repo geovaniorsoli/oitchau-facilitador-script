546f4c9b2c18cee134d1f36151cbfd6809c5cd00
Geovani Orsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-03-26 11:22:35 -0300

    inicial files

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Automação para ponto eletronico na plataforma Oitchau
+Acesse documentação em https://auto-time.vercel.app 
+
+
---
+++ auto.py
@@ -0,0 +1,42 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+
+# dotenv environment
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+user = os.getenv("USER")
+password = os.getenv("PASS")
+web = os.getenv("WEB")
+
+driver = webdriver.Edge()
+
+try:
+    driver.execute_script("window.open('about:blank', '_blank');")
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.get(web) 
+
+    inputs = WebDriverWait(driver, 9000).until(EC.visibility_of_all_elements_located((By.NAME, "email")))
+    password_input = WebDriverWait(driver, 9000).until(EC.visibility_of_element_located((By.NAME, "password")))
+    
+    inputs[0].send_keys(user)
+    password_input.send_keys(password)
+    
+    button = WebDriverWait(driver, 9000).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Acessar painel')]")))
+    button.click()
+
+    button = WebDriverWait(driver, 9000).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Marcar Ponto')]")))
+    button.click()
+
+    button = WebDriverWait(driver, 9000).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'MARCAR PONTO')]")))
+    button.click()
+    
+    time.sleep(5)
+
+finally:
+    driver.quit()
+
